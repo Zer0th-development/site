4a1c1203e64378b9d2cd31d25af213da5faf4d33
Jalil Faquiri <jalil.faquiri@hva.nl>
2024-08-05 16:17:52 +0200

    pls Fix performance

--- src/components/magicui/neon-gradient-card.tsx
+++ src/components/magicui/neon-gradient-card.tsx
@@ -10,6 +10,7 @@ import {
 } from "react";
 
 import { cn } from "../../@/lib/utils";
+import "./neon-gradient-card.css";
 
 interface NeonColorsProps {
   firstColor: string;
@@ -77,33 +78,23 @@ const NeonGradientCard = ({
           "--card-width": `${dimensions.width}px`,
           "--card-height": `${dimensions.height}px`,
           "--card-content-radius": `${borderRadius - borderSize}px`,
-          "--pseudo-element-background-image": `linear-gradient(0deg, ${neonColors.firstColor}, ${neonColors.secondColor})`,
-          "--pseudo-element-width": `${dimensions.width + borderSize * 2}px`,
-          "--pseudo-element-height": `${dimensions.height + borderSize * 2}px`,
           "--after-blur": `${dimensions.width / 3}px`,
           fontFamily: fontFamily
         } as CSSProperties
       }
       className={cn(
         "relative z-10 h-full w-full rounded-[var(--border-radius)]",
+        "neon-gradient-card",
         className
       )}
       {...props}
     >
       <div
         className={cn(
-          "relative h-full min-h-[inherit] w-full rounded-[var(--card-content-radius)] bg-black p-6",
-          "before:absolute before:-left-[var(--border-size)] before:-top-[var(--border-size)] before:-z-10 before:block",
-          "before:h-[var(--pseudo-element-height)] before:w-[var(--pseudo-element-width)] before:rounded-[var(--border-radius)] before:content-['']",
-          "before:bg-[linear-gradient(0deg,var(--neon-first-color),var(--neon-second-color))] before:bg-[length:100%_200%]",
-          "before:animate-backgroundPositionSpin",
-          "after:absolute after:-left-[var(--border-size)] after:-top-[var(--border-size)] after:-z-10 after:block",
-          "after:h-[var(--pseudo-element-height)] after:w-[var(--pseudo-element-width)] after:rounded-[var(--border-radius)] after:blur-[var(--after-blur)] after:content-['']",
-          "after:bg-[linear-gradient(0deg,var(--neon-first-color),var(--neon-second-color))] after:bg-[length:100%_200%] after:opacity-80",
-          "after:animate-backgroundPositionSpin",
+          "relative h-full min-h-[inherit] w-full rounded-[var(--card-content-radius)] bg-[#121212] p-6",
           "dark:bg-[#111111]"
         )}
-        style={{ fontFamily: fontFamily, background: "#121212" }}
+        style={{ fontFamily: fontFamily }}
       >
         {children}
       </div>
